a09d6b046e71337a3fb56862b7acfe1beb9b7c62
unknown <idemaj@rpi.edu>
2022-01-30 11:02:51 -0500

    title tweak

--- Assets/Scripts/TitleManager.cs
+++ Assets/Scripts/TitleManager.cs
@@ -2,10 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class TitleManager : MonoBehaviour
 {
     public string[] scenes;
+
+    public TMP_Text[] highScores;
+
+    private void Start()
+    {
+        for (int i = 0; i < highScores.Length; ++i)
+        {
+            if (PlayerPrefs.HasKey("MaxScene" + (i + 2)))
+                highScores[i].text = "Egg Score: " + PlayerPrefs.GetInt("MaxScene" + (i + 2));
+            else
+                highScores[i].text = "No Egg Score";
+        }
+    }
+
     public void LoadLevel(int level)
     {
         SceneManager.LoadScene(scenes[level]);
