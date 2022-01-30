4ebd86dc3916a5c14ae1d15613ede7fd56a9d09c
Misha Pozdniakov <misha.pozd@gmail.com>
2022-01-30 15:38:22 -0500

    Added an audio mixer for the eggs accross all scenes

--- Assets/Scripts/Eggs/FragileEgg.cs
+++ Assets/Scripts/Eggs/FragileEgg.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class FragileEgg : MonoBehaviour
 {
+    public AudioMixerGroup sfx_group;
     public GameObject broken_egg;
     public AudioClip[] clips;
     private float soundDistance = 8.0f;
@@ -18,6 +20,11 @@ public class FragileEgg : MonoBehaviour
         source.playOnAwake = false;
         source.spatialBlend = 1.0f;
         source.maxDistance = soundDistance;
+        try
+        {
+            source.outputAudioMixerGroup = sfx_group;
+        }
+        finally { }
         Invoke("enable_breaking", 1f);
     }
 
@@ -35,22 +42,24 @@ public class FragileEgg : MonoBehaviour
         if (collision.gameObject.layer == 8) return;
         // if soft, don't crack
         //if (collision.relativeVelocity.magnitude < 2.0) return;
+        if (collision.gameObject.layer == 6) Shatter(); // if the elephant stepped on you, break.
         if (collision.impulse.magnitude < 0.1) return;
         if (container != null && collision.impulse.magnitude < 0.2) return;
         if (collision.gameObject.layer == 7 || collision.gameObject.layer == 8) return; // Container layer and egg layer
 
-        // select a clip to play from the list
-        source.clip = clips[UnityEngine.Random.Range(0, clips.Length - 1)];
-        // play it
-        source.Play();
+        
         // shatter
         Shatter();
     }
 
     public void Shatter()
     {
+        // select a clip to play from the list
+        source.clip = clips[UnityEngine.Random.Range(0, clips.Length - 1)];
+        // play it
+        source.Play();
         // create the two shells
-        Instantiate(broken_egg, transform);
+        Instantiate(broken_egg, transform,true);
         // disable own mesh and colliders
         // GetComponent<SphereCollider>().enabled = false;
         GetComponent<MeshRenderer>().enabled = false;
